76d74e84b2a65534c62885bf3cad3d66cfc80fea
mtkwock <m.t.kwock@gmail.com>
2015-11-02 17:03:50 -0500

    Message interaction for Friends

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
-
+    $("#leoMessage").on("click", function(){
+        $("#name").text("Learning Leo");
+    })
+    $("phoebeMessage").on("click", function(){
+        $("#name").text("Philosophical Phoebe");
+    });
+    $("carlMessage").on("click", function(){
+        $("#name").text("Cultural Carl");
+    });
 });
+
+var sendMessage = function(){
+    bootstrap_alert.success("Message Sent")
+};
+
+var bootstrap_alert = function() {
+
+};
+
+bootstrap_alert.success = function(message) {
+    $('#alert_placeholder').html('<div class="alert alert-success"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>');
+}
