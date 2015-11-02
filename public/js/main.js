c591fa643a89ca219e7794a055ea6cd51c76de91
Thomas Nattestad <thomas.nattestad@gmail.com>
2015-11-02 17:33:50 -0500

    prechat screen layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
+  var peckCounter = 0;
+  $('.peckBox').click(function(){
+    peckCounter += 1;
+    $(this).attr(
+      'src', "http://pngimg.com/upload/pigeon_PNG3426.png"
+    )
+  });
 });
